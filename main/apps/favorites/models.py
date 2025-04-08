e06db3b796480a9b38cf3b404c2cebf02f26dc20
Nguyễn Nhật Khoa <103474813+nhatkhoa0502@users.noreply.github.com>
2025-04-08 17:23:40 +0700

    added: complete api favorite track, favorite album

--- main/apps/favorites/models.py
+++ main/apps/favorites/models.py
@@ -1,18 +1,17 @@
 from django.db import models
+from apps.core.models import BaseModel  # giả sử bạn đặt BaseModel trong apps.core.models
 
-class FavoriteTrack(models.Model):
+class FavoriteTrack(BaseModel):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='favorite_tracks')
     track = models.ForeignKey('tracks.Track', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ('user', 'track')
 
 
-class FavoriteAlbum(models.Model):
+class FavoriteAlbum(BaseModel):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='favorite_albums')
-    album = models.ForeignKey('tracks.Track', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    album = models.ForeignKey('albums.Album', on_delete=models.CASCADE)  
 
     class Meta:
         unique_together = ('user', 'album')
--- main/apps/favorites/serializers.py
+++ main/apps/favorites/serializers.py
@@ -1,12 +1,34 @@
 from rest_framework import serializers
-from .models import FavoriteTrack, FavoriteAlbum
+from apps.favorites.models import FavoriteTrack
+from apps.favorites.models import FavoriteAlbum
+from apps.tracks.models import Track
+from apps.albums.models import Album
+from apps.tracks.serializers import TrackSerializer 
+from apps.albums.serializers import AlbumSerializer
 
 class FavoriteTrackSerializer(serializers.ModelSerializer):
+    track_id = serializers.PrimaryKeyRelatedField(
+        queryset=Track.objects.all(),
+        source='track',
+        write_only=True
+    )
+    track = TrackSerializer(read_only=True)
+
     class Meta:
         model = FavoriteTrack
-        fields = ['id', 'track', 'created_at']
+        fields = ['id', 'track', 'track_id', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
 
 class FavoriteAlbumSerializer(serializers.ModelSerializer):
+    album_id = serializers.PrimaryKeyRelatedField(
+        queryset=Album.objects.all(),
+        source='album',
+        write_only=True
+    )
+    album = AlbumSerializer(read_only=True)  
+
     class Meta:
         model = FavoriteAlbum
-        fields = ['id', 'album', 'created_at']
+        fields = ['id', 'album', 'album_id', 'created_at']
+        read_only_fields = ['id', 'created_at']
