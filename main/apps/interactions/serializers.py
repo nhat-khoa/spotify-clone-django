27fc153792bd272d53d04d949ef6b4b9cc1a959f
nguyen huy khiem <nguyenhuykhiem2111@gmail.com>
2025-05-09 19:50:45 +0700

    updated: enhance artist interaction by adding followers count; refactor serializers and views for improved data retrieval on home page

--- main/apps/interactions/serializers.py
+++ main/apps/interactions/serializers.py
@@ -8,6 +8,11 @@ from apps.users.serializers import UserSerializer
 from apps.users.models import User
 from apps.tracks.serializers import TrackSerializer
 from apps.albums.serializers import AlbumSerializer
+from apps.artists.models import Artist  # Import the Artist model
+from apps.albums.models import Album  # Import the Album model
+from apps.podcasts.models import Podcast, PodcastCategory
+from apps.podcasts.serializers import PodcastEpisodeSerializer, SimplePodcasterSerializer
+from apps.tracks.models import Track
 
 class PlaylistSerializer(serializers.ModelSerializer):
     user_id = serializers.PrimaryKeyRelatedField(source='user', read_only=True)
@@ -124,3 +129,105 @@ class UserSavedEpisodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSavedEpisode
         fields = '__all__'
+        
+        
+# artist, podcast, track, album serializer for interactions
+
+
+
+class SimpleArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = '__all__'  # Hoặc có thể chọn các trường cụ thể
+
+class AlbumInteractionSerializer(serializers.ModelSerializer):
+    artist = SimpleArtistSerializer(read_only=True)
+    is_favorite = serializers.SerializerMethodField()
+    tracks_count = serializers.IntegerField(source='tracks.count', read_only=True)
+
+    class Meta:
+        model = Album
+        fields = '__all__'  # Hoặc có thể liệt kê các trường cụ thể
+
+    def get_is_favorite(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return UserSavedAlbum.objects.filter(user=user, album=obj).exists()
+        return False
+    
+
+    
+
+class ArtistInteractionSerializer(serializers.ModelSerializer):
+    albums = AlbumInteractionSerializer(many=True, read_only=True)  # Assuming you want to show artist's albums
+    is_favorite = serializers.SerializerMethodField()
+    followers = serializers.IntegerField(source='followers.count', read_only=True)
+    class Meta:
+        model = Artist
+        fields = '__all__'  # Hoặc có thể chọn các trường cụ thể
+        
+    def get_is_favorite(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return UserFollowedArtist.objects.filter(user=user, artist=obj).exists()
+        return False
+
+
+class TrackInteractionSerializer(serializers.ModelSerializer):
+    artist = SimpleArtistSerializer(read_only=True)  # Assuming you want to show artist details
+    album = AlbumInteractionSerializer(read_only=True)  # Assuming you want to show album details
+    is_favorite = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Track
+        fields = '__all__'
+        extra_kwargs = {
+            'title': {'required': True},
+            'audio_file_path': {'required': True},
+            'artist': {'required': False},  # Assuming artist is required
+            'album': {'required': False},  # Assuming album is required
+        }
+    
+    def get_is_favorite(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return UserSavedTrack.objects.filter(user=user, track=obj).exists()
+        return False
+        
+
+class PodcastInteractionSerializer(serializers.ModelSerializer):
+    podcaster = SimplePodcasterSerializer(read_only=True)
+    is_favorite = serializers.SerializerMethodField()
+    is_rating = serializers.SerializerMethodField()
+    rate_average = serializers.SerializerMethodField()
+    categories = serializers.SerializerMethodField()
+    episodes_count = serializers.IntegerField(source='episodes.count', read_only=True)
+    class Meta:
+        model = Podcast
+        fields = '__all__'
+        extra_kwargs = {
+            'title': {'required': True},
+            'podcaster': {'required': False}, 
+
+        }
+        
+    def get_is_favorite(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return UserFollowedPodcast.objects.filter(user=request.user, podcast=obj).exists()
+        return False
+    
+    def get_is_rating(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return any(rating['user_id'] == str(request.user.id) for rating in obj.rate)
+        return False
+    
+    def get_rate_average(self, obj):
+        if obj.rate:
+            total_rating = sum(rating['rate'] for rating in obj.rate)
+            return total_rating / len(obj.rate) if len(obj.rate) > 0 else 0
+        return 0
+    
+    def get_categories(self, obj):
+        return PodcastCategory.objects.filter(podcast=obj).select_related('category').values('category__id', 'category__name',)
